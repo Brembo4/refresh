b61e216b600c3acad65732956590c26b40cae8d0
Brembo4 <47564772+Butterf1y420@users.noreply.github.com>
2020-05-29 10:22:24 -0300

    declared and accessed pointer to instance of class Account

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <string>
+
+class Account {
+  //attributes
+  public:
+  std::string name;
+  double balance {0};
+  //methods
+  void withdraw(double);
+  void deposit(double);
+};
 
 int main() {
-  std::cout << "hope i see this" << std::endl;
+  Account *brembo {nullptr};
+  brembo = new Account();
+  //first way, dereferencing, then using dot operator:
+  (*brembo).name = "brembo";
+  (*brembo).balance = 100.0;
+  std::cout << (*brembo).name << " " << (*brembo).balance << std::endl;
+  std::cout << "-----------------------------" << std::endl;
+  //second way, using member of pointer operator (arrow operator)
+  brembo->name = "broucques";
+  brembo->balance = 50.0;
+  std::cout << brembo->name << " " << brembo->balance << std::endl;
+  delete brembo;
   return 0;
 }
